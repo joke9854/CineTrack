d652328b7748487151a0cabf6e92ea1fa76be84f
joke9854 <109229663+joke9854@users.noreply.github.com>
2026-09-13 01:16:01 +0200

    Bump CineTrack to 0.98 beta [release]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.cinetrack"
         minSdk = 23
         targetSdk = 36
-        versionCode = 97
-        versionName = "0.97"
+        versionCode = 98
+        versionName = "0.98"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
