c224e4d104aefb94b8c8ac36dfcac1ade515c55a
joke9854 <109229663+joke9854@users.noreply.github.com>
2026-09-14 22:41:19 +0200

    Release CineTrack 0.99.07 beta [release]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.cinetrack"
         minSdk = 23
         targetSdk = 36
-        versionCode = 105
-        versionName = "0.99.06"
+        versionCode = 106
+        versionName = "0.99.07"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
