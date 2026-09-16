d3055a5d5fdc1c2530ac8cb0063218f3fedb5ead
joke9854 <109229663+joke9854@users.noreply.github.com>
2026-09-16 20:52:13 +0200

    Release CineTrack 0.99.18 beta [release] [skip tests]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.cinetrack"
         minSdk = 23
         targetSdk = 36
-        versionCode = 116
-        versionName = "0.99.17"
+        versionCode = 117
+        versionName = "0.99.18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
